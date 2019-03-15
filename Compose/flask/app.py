2a62cae763c407b06f047ced2e673db7ad6d9642
Deboraarzu <deboranise29@hotmail.com>
2019-03-15 09:57:48 -0600

    Nginx as Proxy server, Redis as BackEnd and Flask.

--- Compose/flask/app.py
+++ Compose/flask/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, Response
-
+from redis import Redis
+import os
 
 app = Flask(__name__)
+redis = Redis(host = 'redis', port=6379)
 
 
 @app.route("/")
 def hello():
-    return Response("Hi from your Flask app running in your Docker container!")
+    redis.incr('hits')
+    return 'Hello from Flask! You have seen this %s times.' % redis.get('hits')
 
 
 if __name__ == "__main__":
